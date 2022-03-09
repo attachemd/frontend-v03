39417a20fca75664f742163b45c0943b79e8756d
attachemd <mattache.vivace@gmail.com>
2022-03-09 22:10:09 +0100

    updated the test

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -29,13 +29,17 @@ describe('License Manager', () => {
       'superadmin@email.com'
     );
   });
-  it('wrong email', () => {
-    // cy.get('[data-testid="email"]').should('have.text', '');
+  it('When typing wrong email, this field should display red outline, and submit button should be disabled', () => {
     cy.get('[data-testid="email"]').clear().type('superadminemail.com');
-    cy.get('[data-testid="password"]').type('string');
+    cy.get('[data-testid="email"]').should('have.class', 'ng-invalid');
     cy.get('[data-testid="submit"]').should('be.disabled');
-    // cy.get('[data-testid="email"]').should('have.text', 'superadmin@email.com');
-    cy.get('[data-testid="email"]').should('have.value', 'superadminemail.com');
+  });
+  it('When password is short, this field should display red outline, and submit button should be disabled', () => {
+    cy.get('[data-testid="password"]').type('val').clear();
+    // cy.get('[data-testid="password"]').invoke('val', '');
+    cy.get('[data-testid="email"]').clear().type('superadmin@email.com');
+    cy.get('[data-testid="password"]').should('have.class', 'ng-invalid');
+    cy.get('[data-testid="submit"]').should('be.disabled');
   });
   it('all pass', () => {
     // cy.get('[data-testid="email"]').should('have.text', '');
