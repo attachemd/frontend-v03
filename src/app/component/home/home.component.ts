e548c31f8750e91423d3604941e48cb46f046e69
attachemd <mattache.vivace@gmail.com>
2022-03-20 23:20:31 +0100

    Updated drag & drop

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DragulaService } from 'ng2-dragula';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent implements OnInit, OnDestroy {
+  constructor(private _dragulaService: DragulaService) {
+    _dragulaService.createGroup('VAMPIRES', {
+      accepts: (el, target, source, sibling) => {
+        console.log(!source?.classList.contains('top'));
+
+        if (!source?.classList.contains('top')) return false;
+        else return true;
+      },
+      copy: true,
+    });
+  }
 
   ngOnInit(): void {
     console.log('HomeComponent');
+  }
+
+  ngOnDestroy(): void {
+    this._dragulaService.destroy('VAMPIRES');
   }
 }
