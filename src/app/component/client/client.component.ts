eb46c1e599277906d85613a8d522adc8d0d00d22
attachemd <mattache.vivace@gmail.com>
2022-04-08 10:25:35 +0000

    refactor: do some cleanup

--- src/app/component/client/client.component.ts
+++ src/app/component/client/client.component.ts
@@ -1,42 +1,18 @@
-import {
-  Component,
-  ComponentFactory,
-  ComponentFactoryResolver,
-  ComponentRef,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-  ViewContainerRef,
-} from '@angular/core';
-import { DnDFieldComponent } from '../dnd-field/dnd-field.component';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
   styleUrls: ['./client.component.scss'],
 })
-export class ClientComponent implements OnInit, OnDestroy {
+export class ClientComponent implements OnInit {
   @ViewChild('alertContainer', { read: ViewContainerRef })
   public viewContainerRef: ViewContainerRef = {} as ViewContainerRef;
 
   public componentRef: any;
-  constructor(private _resolver: ComponentFactoryResolver) {}
-
-  public createComponent(type: string) {
-    this.viewContainerRef.clear();
-    // const factory = this._resolver.resolveComponentFactory(DnDFieldComponent);
-
-    // this.componentRef = this.viewContainerRef.createComponent(factory);
-    this.componentRef =
-      this.viewContainerRef.createComponent(DnDFieldComponent);
-    this.componentRef.instance.type = type;
-  }
+  constructor() {}
 
   ngOnInit(): void {
     console.log('ClientComponent');
-  }
-
-  ngOnDestroy() {
-    this.componentRef?.destroy();
   }
 }
